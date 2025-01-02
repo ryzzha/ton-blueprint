2315540d6c35ebbf4a939248dbd8cf2d7605e726
IraRyzha <ira.ryzha2803@gmail.com>
2025-01-02 16:43:35 +0200

    ;)

--- main.ts
+++ main.ts
@@ -6,11 +6,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const seedPhrase = process.env.SEED_PHRASE;
-console.log('seedPhrase:', seedPhrase);
 
 async function main() {
-    const mnemonic = seedPhrase;
-    const key = await mnemonicToWalletKey(mnemonic.split(" "));
+    const key = await mnemonicToWalletKey(seedPhrase.split(" "));
     const wallet = WalletContractV4.create({ publicKey: key.publicKey, workchain: 0  });
 
     const endpoint = await getHttpEndpoint({ network: "testnet" });
@@ -20,13 +18,10 @@ async function main() {
         console.log("wallet is not deployed");
     }
 
-    console.log("wallet exist address: " + wallet.address);
-    console.log("wallet balance: " + fromNano(await client.getBalance(wallet.address)));
-
-    // EQAJu_Cc2RKOoRaRTFOat8XIMJ8OFpTEkY2SunELLP5V4PWF  EQA4u5b8h39SxZpbAO8I7lDfEwVKqb7mtZPRqqKqFeumrkh3
-
     const walletContract = client.open(wallet);
     const seqno = await walletContract.getSeqno();
+
+    console.log(JSON.stringify(walletContract, null, 2));
 
     await walletContract.sendTransfer({
         secretKey: key.secretKey,
@@ -35,13 +30,11 @@ async function main() {
            internal({
             to: "EQAQlYCcZn_9_dizmW7KGvAyVgK6hcG8QQxIz3QiMp5BSGSX",
             value: "0.05",
-            body: "hello",
+            body: ":)",
             bounce: false
            })
         ]
     });
-
-    console.log("wallet seqno: " + seqno);
 
     let currentSeqno = seqno;
     while(currentSeqno == seqno) {
